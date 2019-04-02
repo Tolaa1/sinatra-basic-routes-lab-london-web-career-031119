ead661312492658078fe8fdd085e41f8bc06f3a5
Tola <you@example.com>
2019-04-02 14:12:14 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is tola"
+end
+
+get '/hometown' do
+  "My hometown is ibadan"
+end
+
+get '/favorite-song' do
+  "My favorite song is affrobeat"
+end
 end
